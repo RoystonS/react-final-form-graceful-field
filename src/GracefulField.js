17b27d9df3cbf5822a64ded9042e288b71825895
Andy Edwards <jedwards@fastmail.com>
2022-07-12 12:14:34 -0500

    chore: fix stupid flow errors

--- src/GracefulField.js
+++ src/GracefulField.js
@@ -28,88 +28,90 @@ export type GracefulFieldProps = {
   subscription?: FieldSubscription,
 }
 
-const GracefulField = React.forwardRef<any, GracefulFieldProps>(
-  function GracefulField(
-    {
-      afterSubmit,
-      allowNull,
-      beforeSubmit,
-      children,
-      component,
-      render,
-      data,
-      defaultValue,
-      format,
-      initialValue,
-      invalidValue,
-      isEqual,
-      multiple,
-      name,
-      parse,
-      subscription,
-      type,
-      validate,
-      validateFields,
-      value,
-      ...rest
-    }: GracefulFieldProps,
-    ref: any
-  ): React.Node {
-    if (!name) {
-      throw new Error(
-        'prop name cannot be undefined in <GracefulField> component'
-      )
-    }
-
-    const field: FieldRenderProps = useGracefulField(name, {
-      afterSubmit,
-      allowNull,
-      beforeSubmit,
-      children,
-      component,
-      data,
-      defaultValue,
-      format,
-      initialValue,
-      isEqual,
-      multiple,
-      parse,
-      subscription,
-      type,
-      validate,
-      validateFields,
-      value,
-    })
-
-    if (typeof children === 'function') {
-      return (children: Function)({ ...field, ...rest })
-    }
-
-    if (typeof component === 'string') {
-      // ignore meta, combine input with any other props
-      // eslint-disable-next-line react/no-children-prop
-      return React.createElement((component: any), {
-        ...field.input,
-        children,
-        ref,
-        ...rest,
-      })
-    }
-
-    if (component) {
-      return React.createElement(component, { ref, ...rest, ...field })
-    }
-    if (typeof render !== 'function') {
-      throw new Error(
-        `Must specify either a render prop, a render function as children, or a component prop to GracefulField(${name})`
-      )
-    }
-    return render(
-      children === undefined
-        ? { ref, ...rest, ...field }
-        : { ref, ...rest, ...field, children }
+const GracefulField: React.ComponentType<GracefulFieldProps> = React.forwardRef<
+  any,
+  GracefulFieldProps
+>(function GracefulField(
+  {
+    afterSubmit,
+    allowNull,
+    beforeSubmit,
+    children,
+    component,
+    render,
+    data,
+    defaultValue,
+    format,
+    initialValue,
+    invalidValue,
+    isEqual,
+    multiple,
+    name,
+    parse,
+    subscription,
+    type,
+    validate,
+    validateFields,
+    value,
+    ..._rest
+  }: GracefulFieldProps,
+  ref: any
+): React.Node {
+  const rest: any = _rest
+  if (!name) {
+    throw new Error(
+      'prop name cannot be undefined in <GracefulField> component'
     )
   }
-)
+
+  const field: FieldRenderProps = useGracefulField(name, {
+    afterSubmit,
+    allowNull,
+    beforeSubmit,
+    children,
+    component,
+    data,
+    defaultValue,
+    format,
+    initialValue,
+    isEqual,
+    multiple,
+    parse,
+    subscription,
+    type,
+    validate,
+    validateFields,
+    value,
+  })
+
+  if (typeof children === 'function') {
+    return (children: Function)({ ...field, ...rest })
+  }
+
+  if (typeof component === 'string') {
+    // ignore meta, combine input with any other props
+    // eslint-disable-next-line react/no-children-prop
+    return React.createElement((component: any), {
+      ...field.input,
+      children,
+      ref,
+      ...rest,
+    })
+  }
+
+  if (component) {
+    return React.createElement(component, { ref, ...rest, ...field })
+  }
+  if (typeof render !== 'function') {
+    throw new Error(
+      `Must specify either a render prop, a render function as children, or a component prop to GracefulField(${name})`
+    )
+  }
+  return render(
+    children === undefined
+      ? { ref, ...rest, ...field }
+      : { ref, ...rest, ...field, children }
+  )
+})
 
 export default GracefulField
